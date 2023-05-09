ca6ef8b3fa4f453ecaf1fe653bda05b382698bf6
Oussama Et-talib <119271464+easy-c@users.noreply.github.com>
2023-05-09 18:35:44 +0100

    Add files via upload

--- master.js
+++ master.js
@@ -1,8 +1,12 @@
-let searchPage = document.querySelector(".search-page");
 let searchBar = document.querySelector(".search-bar");
+let searchBarFooter = document.querySelector(".search-bar-footer");
+let searchPage = document.querySelector(".search-page");
 let cancleBtn = document.querySelector("#cancle-btn");
 
 searchBar.onclick = function () {
+    searchPage.style.display = "flex"
+}
+searchBarFooter.onclick = function () {
     searchPage.style.display = "flex"
 }
 cancleBtn.onclick = function () {
