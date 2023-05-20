4799475931cdfc81a1f0c44cf276bd61374f956a
Oussama Et-talib <119271464+easy-c@users.noreply.github.com>
2023-05-20 20:50:13 +0100

    Add files via upload

--- master.js
+++ master.js
@@ -1,15 +1,26 @@
-let searchBar = document.querySelector(".search-bar");
-let searchBarFooter = document.querySelector(".search-bar-footer");
-let searchPage = document.querySelector(".search-page");
-let cancleBtn = document.querySelector("#cancle-btn");
+// Search Bar Page
+    let searchBar = document.querySelector(".search-bar");
+    let searchBarFooter = document.querySelector(".search-bar-footer");
+    let searchPage = document.querySelector(".search-page");
+    let cancleBtn = document.querySelector("#cancle-btn");
 
-searchBar.onclick = function () {
-    searchPage.style.display = "flex"
-}
-searchBarFooter.onclick = function () {
-    searchPage.style.display = "flex"
-}
-cancleBtn.onclick = function () {
-    searchPage.style.display = "none"
-}
+    searchBar.onclick = function () {
+        searchPage.style.display = "flex"
+    }
+    searchBarFooter.onclick = function () {
+        searchPage.style.display = "flex"
+    }
+    cancleBtn.onclick = function () {
+        searchPage.style.display = "none"
+    }
+
+    
+// Back Btn From Project Info 
+    let back = document.querySelector(".back");
+
+    back.onclick = function(){
+    history.back();
+    }
+
+
 
