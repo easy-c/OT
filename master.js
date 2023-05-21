a707611043dd183577a7bdaf9c0efdcf10498360
Oussama Et-talib <119271464+easy-c@users.noreply.github.com>
2023-05-21 17:04:18 +0100

    Add files via upload

--- master.js
+++ master.js
@@ -1,26 +1,48 @@
-// Search Bar Page
-    let searchBar = document.querySelector(".search-bar");
-    let searchBarFooter = document.querySelector(".search-bar-footer");
-    let searchPage = document.querySelector(".search-page");
-    let cancleBtn = document.querySelector("#cancle-btn");
+// // Search Bar Page
+//     let searchBar = document.querySelector(".search-bar");
+//     let searchBarFooter = document.querySelector(".search-bar-footer");
+//     let searchPage = document.querySelector(".search-page");
+//     let cancleBtn = document.querySelector("#cancle-btn");
 
-    searchBar.onclick = function () {
-        searchPage.style.display = "flex"
-    }
-    searchBarFooter.onclick = function () {
-        searchPage.style.display = "flex"
-    }
-    cancleBtn.onclick = function () {
-        searchPage.style.display = "none"
-    }
+//     searchBar.onclick = function () {
+//         searchPage.style.display = "flex"
+//     }
+//     searchBarFooter.onclick = function () {
+//         searchPage.style.display = "flex"
+//     }
+//     cancleBtn.onclick = function () {
+//         searchPage.style.display = "none"
+//     }
+
+
+// // Back Btn From Project Info 
+//     let back = document.querySelector(".back");
+
+//     back.onclick = function () {
+//         console.log("Back button clicked");
+//         history.back();
+//     }
+
+
+// Search Bar Page
+const searchPage = document.querySelector(".search-page");
+const cancleBtn = document.querySelector("#cancle-btn");
+
+document.addEventListener("click", function (event) {
+if (event.target.matches(".search-bar, .search-bar-footer")) {
+    searchPage.style.display = "flex";
+} else if (event.target === cancleBtn) {
+    searchPage.style.display = "none";
+}
+});
+
+// Back Btn From Project Info
+const back = document.querySelector(".back");
+
+back.addEventListener("click", function () {
+history.back();
+});
 
     
-// Back Btn From Project Info 
-    let back = document.querySelector(".back");
-
-    back.onclick = function(){
-    history.back();
-    }
-
 
 
