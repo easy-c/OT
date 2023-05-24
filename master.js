2af823a14915b9ec207908a21708307bedaab172
Oussama Et-talib <119271464+easy-c@users.noreply.github.com>
2023-05-24 17:17:07 +0100

    Add files via upload

--- master.js
+++ master.js
@@ -1,48 +1,30 @@
-// // Search Bar Page
-//     let searchBar = document.querySelector(".search-bar");
-//     let searchBarFooter = document.querySelector(".search-bar-footer");
-//     let searchPage = document.querySelector(".search-page");
-//     let cancleBtn = document.querySelector("#cancle-btn");
-
-//     searchBar.onclick = function () {
-//         searchPage.style.display = "flex"
-//     }
-//     searchBarFooter.onclick = function () {
-//         searchPage.style.display = "flex"
-//     }
-//     cancleBtn.onclick = function () {
-//         searchPage.style.display = "none"
-//     }
-
-
-// // Back Btn From Project Info 
-//     let back = document.querySelector(".back");
-
-//     back.onclick = function () {
-//         console.log("Back button clicked");
-//         history.back();
-//     }
-
-
-// Search Bar Page
-const searchPage = document.querySelector(".search-page");
-const cancleBtn = document.querySelector("#cancle-btn");
-
 document.addEventListener("click", function (event) {
-if (event.target.matches(".search-bar, .search-bar-footer")) {
+    // Search Bar Page
+    if (event.target.matches(".search-bar, .search-bar-footer")) {
+    const searchPage = document.querySelector(".search-page");
     searchPage.style.display = "flex";
-} else if (event.target === cancleBtn) {
+    } else if (event.target.matches("#cancel-btn")) {
+    const searchPage = document.querySelector(".search-page");
     searchPage.style.display = "none";
-}
+    }
+
+    // Back Btn From Project Info
+    if (event.target.matches(".back")) {
+    history.back();
+    }
+
+    // See More Of Projects
+    if (event.target.matches("#seeMore")) {
+    const hiddenItemList = document.querySelectorAll(".hiddenItem");
+    hiddenItemList.forEach(function (hiddenItem) {
+        if (hiddenItem.style.display === "none") {
+        hiddenItem.style.display = "block";
+        event.target.textContent = "SEE LESS";
+        } else {
+        hiddenItem.style.display = "none";
+        event.target.textContent = "SEE MORE";
+        }
+    });
+    }
+
 });
-
-// Back Btn From Project Info
-const back = document.querySelector(".back");
-
-back.addEventListener("click", function () {
-history.back();
-});
-
-    
-
-
